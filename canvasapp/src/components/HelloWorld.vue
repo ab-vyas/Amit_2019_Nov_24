990883b1f6e9d1fb3e8cf257c3af9088cdbf20c9
Amit Vyas <abvyas88@gmail.com>
2019-11-25 03:41:04 +0530

    Adding files and get data from API.

--- canvasapp/src/components/HelloWorld.vue
+++ canvasapp/src/components/HelloWorld.vue
@@ -1,40 +1,31 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    {{ this.picData }}
   </div>
 </template>
 
 <script>
+import {getIdList, fetchPhoto, chunks } from '../helpers/helper.js';
+//const { getIdList, fetchPhoto, chunks } = require('./helpers/helper');
+const t0 = Date.now();
+var picData  = [];
+chunks(getIdList(200), fetchPhoto, 10)
+.then(res=> {
+    const t1 = Date.now();
+    /* eslint-disable no-console */
+    console.log(`Fetch time: ${t1 - t0} ms`);
+    /* eslint-disable no-console */
+    console.log(`Response ${res} ms`);
+    //tData.push(JSON.stringify(res));
+    picData.push(res);
+console.log(picData[0]);
+});
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    picData: Array
   }
 }
 </script>
